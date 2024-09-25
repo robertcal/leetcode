f86cc43d8f1f69ede235aabb55a78c5c93fe4e2c
robertcal <robertcal900@gmail.com>
2024-09-25 22:15:12 +0900

    Add 409-longest-palindrome other solutions

---
+++ 409-longest-palindrome/main3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        seen = set()
+        res = 0
+
+        for c in s:
+            if c in seen:
+                seen.remove(c)
+                res += 2
+            else:
+                seen.add(c)
+
+        if seen: # not empty
+            res += 1
+
+        return res
